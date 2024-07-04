67a09e7513a7ff97654fbd87b0a777e9b96b8b1d
Crowdbotics <team@crowdbotics.com>
2024-07-04 07:00:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-feather-48624",
+  url: "https://falling-feather-48624.botics.co",
+  api: "https://falling-feather-48624.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
